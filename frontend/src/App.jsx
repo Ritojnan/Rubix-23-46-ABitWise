d6cbc4742d0017338fc39c0e528d646309515244
Ritojnan <ritojnanm+work@gmail.com>
2023-01-18 19:55:57 +0530

    Added required npm dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Chatbox from './components/Chatbot.jsx'
+
+import { useRef, useState } from "react";
+// import "bootstrap/dist/css/bootstrap.css";
+import "./custom.scss";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { motion } from "framer-motion";
 function App() {
   const [count, setCount] = useState(0)
 
