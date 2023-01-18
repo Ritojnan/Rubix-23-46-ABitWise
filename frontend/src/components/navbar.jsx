1515d4b0530dd4704eb2eecc195218629de5913d
Tanmay550 <tanmaysarode2005@gmail.com>
2023-01-18 22:10:15 +0530

    completed navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,50 +6,51 @@ import Col from "react-bootstrap/Col";
 export default function Navbar() {
   return (
     <>
+       
       <header className="p-3 text-bg-dark">
+        
         <div className="container">
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a
               href="/"
               className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
             >
-              {/* <svg
-                className="bi me-2"
-                width="40"
-                height="32"
-                role="img"
-                aria-label="Bootstrap"
-              >
-                <use xlink:href="#bootstrap"></use>
-              </svg> */}
+              <div className="text-start "><b>resQuick</b></div>
             </a>
+            
 
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              
               <li>
-                <a href="#" className="nav-link px-2 text-secondary">
-                  Home
+                <a href="#" className="nav-link px-2 text-white">
+                  Volunteer
                 </a>
               </li>
               <li>
                 <a href="#" className="nav-link px-2 text-white">
-                  Features
+                  Donation
                 </a>
               </li>
               <li>
                 <a href="#" className="nav-link px-2 text-white">
-                  Pricing
+                  Webinar
+                </a>
+              </li>
+              {/* <li>
+                <a href="#" className="nav-link px-2 text-white">
+                  Translate
                 </a>
               </li>
               <li>
                 <a href="#" className="nav-link px-2 text-white">
-                  FAQs
+                  Flashlight
                 </a>
               </li>
               <li>
                 <a href="#" className="nav-link px-2 text-white">
-                  About
+                  Siren
                 </a>
-              </li>
+              </li> */}
             </ul>
 
             
